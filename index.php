fef24c7cecc7dce3ee33db16be0694380a1ee098
formidablae <81068781+formidablae@users.noreply.github.com>
2021-11-10 19:44:41 +0100

    Fixing typos

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     $name = "PHP Store";
     $credit = 1000;
 
-    echo "<h1>Welcome to ".$name."!</h1>";
-    echo "<h2>You have $".$credit." in your wallet.</h2>";";
+    echo "<h1>Welcome to " . $name . "!</h1>";
+    echo "<h2>You have $" . $credit . " in your wallet.</h2>";
     ?>
   </body>
 </html>
